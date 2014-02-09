29e3aff5e4ffdaac135217db6f11e7bb471bf8f4
Deaygo <deaygo@thezomg.com>
2014-02-09 17:17:04 -0500

    Convert config to YAML file

--- bot.rb
+++ bot.rb
@@ -1,13 +1,22 @@
 require 'cinch'
 require './plugins/tell.rb'
+require 'yaml'
 
 bot = Cinch::Bot.new do
     configure do |c|
-        c.nick = 'BlubberBot'
-        c.server = 'irc.gamesurge.net'
-        c.channels = ['#llama5']
-        # c.verbose = true
-        c.plugins.plugins = [Tell]
+        begin
+            conf = YAML.load_file('config.yaml')
+            c.load(conf)
+        rescue
+            ## Default config
+            c.nick = 'BlubberBot'
+            c.server = 'irc.gamesurge.net'
+            c.channels = ['#llama5']
+            #c.verbose = true
+            c.plugins.plugins = [Tell]
+            File.open('config.yaml', 'w') { |fo| fo.puts c.to_h.to_yaml }
+            abort("Config doesn't exist, creating config.yaml")
+        end
     end
 end
 
